6c303e9ca73e3c2cdd5d461c47a6f82c98b091fd
root <root@VM_52_170_centos.(none)>
2017-04-06 23:27:33 +0800

    修改相关信息

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,24 +1,90 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-/**
- * Created by PhpStorm.
- * User: zuolinux.com
- * Date: 2017/1/11
- * Time: 11:22
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to download</title>
 
-class Home extends CI_Controller{
-    public function index(){
+	<style type="text/css">
 
-        $file_list=array();
+.logo { width:1300px;height:100px;border:0px solid #000; text-align:center;}
 
-        foreach( glob(SAVE_PATH . "*") as $filename ){
+      /*  body{TEXT-ALIGN: center;}*/
 
-            array_push($file_list, array(str_replace(SAVE_PATH, "", $filename), filesize($filename)));
-
+        #shuru{
+            position: absolute;
+            top:0;
+            left:0;
+            bottom:0;
+            right:0;
+            width:50%;
+            height:30%;
+            margin:auto;
+            border:0px solid;
+            text-align: center;
+            vertical-align:middle;
+            line-height:10px;
         }
 
-        $data['file_list'] = $file_list;
+    </style>
 
-        $this->load->view('home', $data);
-    }
-}
+    <script language="javascript">
+        host = window.location.host;
+        //document.write("<br>host="+host)
+    </script>
+
+</head>
+<body>
+
+<!--
+<div id="form1">
+<form method="post" action="/download/">
+
+	<input type="text" name="url">
+    <input type="submit" value="submit">
+
+</form>
+</div>
+-->
+
+<br/>
+<br/>
+<br/>
+<div class="logo">
+<img src="/www/logo/logo.png"  alt="logo.png" />
+<a href="https://github.com/zuolinux/lxdl" target="_blank">github</a>
+</div>
+
+
+    <div id="shuru">
+        <form method="post" action="/download/">
+
+            <input type="text" name="url" style ="width:500px;height:20px;" id ="tex" >&nbsp;&nbsp;&nbsp;
+            <input type="submit" value="submit">
+
+        </form>
+<br/><br/><br/>
+
+<br/>
+        <div id="box">
+
+            目前服务上文件列表：<br/><br/>
+            <?php foreach ($file_list as $file){ ?>
+                <a href='http://www.lxdl.tk/www/files/<?php echo $file[0] ?> '>
+
+                    <?php echo $file[0] ?>
+
+                </a>
+
+                &nbsp;&nbsp;文件大小:&nbsp;&nbsp;<?php echo $file[1] ?>
+                <br/><br/>
+
+            <?php } ?>
+        </div>
+
+        <script>
+            document.getElementById('box').innerHTML = document.getElementById('box').innerHTML.replace(/www.lxdl.tk/g, host)
+        </script>
+    </div>
+
+</body>
+</html>
